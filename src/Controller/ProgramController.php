b4462e6c23df7a7e157b092cd525c456c07d01d6
gounou <edwige.gounou@gmail.com>
2021-01-09 18:22:01 +0100

    quete22

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,6 +7,8 @@ use App\Entity\Program;
 use App\Entity\Season;
 use App\Form\CommentType;
 use App\Form\ProgramType;
+use App\Form\SearchProgramType;
+use App\Repository\ProgramRepository;
 use App\Service\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,14 +27,23 @@ Class ProgramController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(Request $request, ProgramRepository $programRepository): Response
     {
-        $programs = $this->getDoctrine()
-        ->getRepository(Program::class)
-        ->findAll();
+        $form = $this->createForm((SearchProgramType::class));
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $search = $form->getData()['search'];
+            $programs = $programRepository->findLikeName($search);
+        } else {
+            $programs = $programRepository->findAll();
+        }
 
         return $this->render('program/index.html.twig',
-            ['programs' => $programs]
+            [
+                'programs' => $programs,
+                'form'=>$form->createView(),
+                ]
         );
     }
 
